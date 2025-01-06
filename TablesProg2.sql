4c889ee92cc5a24d511834f91bbc686c6aa803fb
Aspasia Plangesi <t8230122@aueb.gr>
2025-01-06 23:39:00 +0200

    Update TablesProg2.sql

--- TablesProg2.sql
+++ TablesProg2.sql
@@ -1,27 +1,29 @@
+USE sql7755696;
+
 CREATE TABLE Patient (
-AMKA varchar NOT NULL PRIMARY KEY
-Name varchar(40) NOT NULL
-Surname varchar(40) NOT NULL
-dateOfBirth date NOT NULL
-phoneNumber varchar NOT NULL
-adress varchar(225) NOT NULL
-email varchar NOT NULL
-medicalRecord varchar
+AMKA varchar(9) NOT NULL PRIMARY KEY,
+NameP varchar(40) NOT NULL,
+Surname varchar(40) NOT NULL,
+dateOfBirth date NOT NULL,
+phoneNumber varchar(12) NOT NULL,
+addressP varchar(225) NOT NULL,
+email varchar(50) NOT NULL,
+medicalRecord varchar(1000),
 gender char(1)
-)
+);
 
 CREATE TABLE Doctor(
-docCode varchar(40) NOT NULL PRIMARY KEY
-name varchar(40)
-surname varchar(40)
-specialization varchar(40)
+docCode varchar(40) NOT NULL PRIMARY KEY,
+nameD varchar(40),
+surname varchar(40),
+specialization varchar(40),
 availableTime time
-)
+);
 
 CREATE TABLE Appointment(
-docCode REFERENCES Doctor(docCode)
-specialization varchar(40)
-apptTime time
-apptDate date
+doctCode varchar(40) REFERENCES Doctor(docCode),
+specialization varchar(40),
+apptTime time,
+apptDate date,
 patientName varchar(40) REFERENCES Patient(Name)
-)
+);

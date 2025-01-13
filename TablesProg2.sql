6d7ce085ca1596b520955ce8a8eaa81e36d7490b
Thodoris Taloumis <t8220145@aueb.gr>
2025-01-13 19:34:11 +0200

    Add files via upload

--- TablesProg2.sql
+++ TablesProg2.sql
@@ -1,29 +1,30 @@
-USE sql7755696;
-
 CREATE TABLE Patient (
-AMKA varchar(9) NOT NULL PRIMARY KEY,
-NameP varchar(40) NOT NULL,
-Surname varchar(40) NOT NULL,
-dateOfBirth date NOT NULL,
-phoneNumber varchar(12) NOT NULL,
-addressP varchar(225) NOT NULL,
-email varchar(50) NOT NULL,
-medicalRecord varchar(1000),
-gender char(1)
+    AMKA varchar NOT NULL PRIMARY KEY,
+    NameP varchar(40) NOT NULL,
+    Surname varchar(40) NOT NULL,
+    dateOfBirth date NOT NULL,
+    phoneNumber varchar NOT NULL,
+    addressP varchar(225) NOT NULL,
+    email varchar NOT NULL,
+    medicalRecord varchar,
+    gender char(1)
 );
 
 CREATE TABLE Doctor(
-docCode varchar(40) NOT NULL PRIMARY KEY,
-nameD varchar(40),
-surname varchar(40),
-specialization varchar(40),
-availableTime time
+    docCode varchar(40) NOT NULL PRIMARY KEY,
+    name varchar(40),
+    surname varchar(40),
+    specialization varchar(40),
+    availableTime time,
+    availableMinutes int
 );
 
 CREATE TABLE Appointment(
-doctCode varchar(40) REFERENCES Doctor(docCode),
-specialization varchar(40),
-apptTime time,
-apptDate date,
-patientName varchar(40) REFERENCES Patient(Name)
+    docCode REFERENCES Doctor(docCode),
+    specialization varchar(40),
+    apptTime time,
+    apptDate date,
+    patientAMKA varchar(40) REFERENCES Patient(AMKA),
+    priority INT,
+    duration INT
 );
--- database_schema_and_queries.sql
+++ database_schema_and_queries.sql
@@ -1,32 +1,35 @@
 
 CREATE TABLE Patient (
-    AMKA varchar(20) NOT NULL PRIMARY KEY,
-    Name varchar(40) NOT NULL,
+    AMKA varchar NOT NULL PRIMARY KEY,
+    NameP varchar(40) NOT NULL,
     Surname varchar(40) NOT NULL,
     dateOfBirth date NOT NULL,
-    phoneNumber varchar(20) NOT NULL,
-    address varchar(225) NOT NULL,
-    email varchar(100) NOT NULL,
-    medicalRecord text,
+    phoneNumber varchar NOT NULL,
+    addressP varchar(225) NOT NULL,
+    email varchar NOT NULL,
+    medicalRecord varchar,
     gender char(1)
 );
 
 
-CREATE TABLE Doctor (
+CREATE TABLE Doctor(
     docCode varchar(40) NOT NULL PRIMARY KEY,
-    name varchar(40) NOT NULL,
-    surname varchar(40) NOT NULL,
-    specialization varchar(40) NOT NULL,
-    availableTime time
+    name varchar(40),
+    surname varchar(40),
+    specialization varchar(40),
+    availableTime time,
+    availableMinutes int
 );
 
 
 CREATE TABLE Appointment (
-    docCode varchar(40) NOT NULL,
-    specialization varchar(40) NOT NULL,
-    apptTime time NOT NULL,
-    apptDate date NOT NULL,
-    patientAMKA varchar(20) NOT NULL,
+    docCode REFERENCES Doctor(docCode),
+    specialization varchar(40),
+    apptTime time,
+    apptDate date,
+    patientAMKA varchar(40) REFERENCES Patient(AMKA),
+    priority INT,
+    duration INT,
     
     -- Foreign key references
     FOREIGN KEY (docCode) REFERENCES Doctor(docCode),
